22eccad8f7ebdb91124d974710989de13ad10827
habogay <habogay@gmail.com>
2015-06-21 23:18:37 +0700

    add mobile project

---
+++ src/main/java/ohm/unittest/App.java
@@ -0,0 +1,17 @@
+package ohm.unittest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    public static String abc()
+    {
+    	return "abc";
+    }
+}
---
+++ src/test/java/ohm/unittest/AppTest.java
@@ -0,0 +1,21 @@
+package ohm.unittest;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class AppTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+	@Test
+	public void testAbc()
+	{
+		assertEquals("abc",App.abc());
+	}
+}
